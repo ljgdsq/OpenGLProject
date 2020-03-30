809cb5b447af9d144e4a56bb17c3b6997380164f
zsl <zhengshulin@libii.com>
2020-03-30 15:07:49 +0800

    添加输入事件检测

--- OpenGLProject/main.cpp
+++ OpenGLProject/main.cpp
@@ -11,6 +11,12 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
     glViewport(0, 0, width, height);
 }
 
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
 int main()
 {
     const char* title = "OpenGL";
@@ -45,6 +51,7 @@ int main()
 
     while (!glfwWindowShouldClose(window))
     {
+        processInput(window);
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
